eb1398bb1d4da460080c2e4be21006941a7bf35b
adhasatria <adhasatria24@gmail.com>
2026-05-26 20:48:21 +0800

    jurnal sementara modul 6

--- Module-6/soal_6.cpp
+++ Module-6/soal_6.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <climits>
 using namespace std;
+ 
+int cek_nim(char *argv[]){
+    int panjang=0;
+    
+    while(*argv[panjang]!= '\0'){
+        panjang++;
+    }
+    
+    if(panjang != 11){
+        cout<<"Error: Operator ID must be exactly 11 characters long."<<endl;
+    }else if(*argv[0]== 'F'){
+        if(*argv[1]== '1'){
+            if(*argv[2]== 'D'){
+                if(*argv[3]== '0'){
+                    if(*argv[4]== '2'){
+                        system("clear");
+                    }
+                }else{
+                    cout<<"Error: Operator ID must start with 'F1D02'."<<endl;
+                    return 0;
+                }
+            }else{
+                cout<<"Error: Operator ID must start with 'F1D02'."<<endl;
+                return 0;
+            }
+        }else{
+                cout<<"Error: Operator ID must start with 'F1D02'."<<endl;
+                return 0;
+            }
+    }else{
+                cout<<"Error: Operator ID must start with 'F1D02'."<<endl;
+                return 0;
+            }
 
-int main(){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    if(argc < 2){
+        cout<<"Error: Neural Link requires an Operator ID (Student ID)."<<endl;
+    }else if(argc > 2){
+        cout<<"Error: Too many parameters. Connection unstable."<<endl;
+    }
 
     return 0;
 }
