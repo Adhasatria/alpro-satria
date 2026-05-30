a0b82488fac90048de787957e650d02b7eeb4fdb
adhasatria <adhasatria24@gmail.com>
2026-05-30 13:09:30 +0800

    menyelesaikan commit

--- Module-6/trialmodul6.cpp
+++ Module-6/trialmodul6.cpp
@@ -32,7 +32,9 @@ string getCurrentTimestamp() {
 struct Commit{
     string message;
     string hash;
-    char author[100];
+    string author;
+    string time;
+    Commit* parent=nullptr;
 };
 struct Branch{
     string nama;
@@ -48,38 +50,67 @@ struct Repository{
 };
 
 
-void nama_author(const char *ikan){
-    Commit author;
-    cout<<COLOR_CYAN " GITSIM "<<COLOR_RESET<<"- Lightweight Git Simulator"<<endl;    
-    cout<<COLOR_GRAY<<"Author :"<<COLOR_RESET;
-    for(int i=0; ikan[i]!='\0' ; i++ ){
-    author.author[i]=ikan[i];
+void commit(Repository* repo, string author){
+    Branch* branch_aktif = &(repo->brach);
+    string pesan_sementara;
+    char konfirmasi; 
+    Commit waktu;
+
+    cout << "git commit [" << branch_aktif->nama << "]" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "Message : ";
+    getline(cin, pesan_sementara);
+
+    cout << "Push commit? (y/n) ";
+    cin >> konfirmasi;
+    cin.ignore(); 
+    if(konfirmasi == 'y' || konfirmasi == 'Y'){
+        waktu.time= getCurrentTimestamp();
+        Commit* new_commit = new Commit;
+        new_commit->message = pesan_sementara;
+        new_commit->author = string();
+        new_commit->hash = generateShortHash(branch_aktif->commit_count + 1);
+        
+        new_commit->parent = branch_aktif->head_commit;
+        
+        branch_aktif->head_commit = new_commit;
+        branch_aktif->commit_count++;
+
+        cout << COLOR_GREEN << "[" << branch_aktif->nama << " " << new_commit->hash << "] " << COLOR_RESET << pesan_sementara << endl;
+        cout << COLOR_GREEN << branch_aktif->nama << " -> origin/" << branch_aktif->nama << COLOR_RESET << endl;
+        cout << " $ git push origin " << branch_aktif->nama << endl;
+    } else {
+        cout << COLOR_RED << "[BATAL] Commit dibatalkan." << COLOR_RESET << endl;
     }
-    cout<<author.author<<endl;
 }
-void commit(Repository* repo, const char *ikan){
-    Commit* new_commit = new Commit;
-    Branch* new_commit = new Branch;
-    cout<<COLOR_CYAN " GITSIM "<<COLOR_RESET<<"- Lightweight Git Simulator"<<endl;    
-    cout<<COLOR_GRAY<<"Author :"<<COLOR_RESET;
-    nama_author(ikan);
-    new_commit.
-}
+
 
 int main(int argc, char *argv[])
 {   Repository* repo =  new Repository;
     string nama_sementara;
+    string nama_author=argv[1];
 
-    nama_author(argv[1]);
+
+    cout<<COLOR_CYAN " GITSIM "<<COLOR_RESET<<"- Lightweight Git Simulator"<<endl;    
+    cout<<COLOR_GRAY<<"Author :"<<COLOR_RESET<<nama_author<<endl;
     cout<<"--------------------------------------------"<<endl;
     cout<<"git init"<<endl;
     cout<<"--------------------------------------------"<<endl;
-    cout<<COLOR_CYAN " Repository name :"<<COLOR_RESET;getline(cin, nama_sementara);
+    cout<<COLOR_CYAN " Repository name :"<<COLOR_RESET;cin.ignore(); getline(cin, nama_sementara);
     if(nama_sementara.empty()){
         repo->name= "my-repo";
     }else{
         repo->name= nama_sementara;
     }
     cout<<COLOR_GREEN "[OK]" COLOR_RESET<<"Initialized empty repository: "<<repo->name;
+    
+    repo->brach.nama = "main";
+    repo->brach.head_commit = nullptr;
+    repo->brach.commit_count = 0;
+
+    cout << COLOR_GREEN "[OK] " COLOR_RESET << "Initialized empty repository: " << repo->name << endl;
+    cout << "On branch: main\n" << endl;
+    
+    commit(repo, argv[1]);
     return 0;
 }
